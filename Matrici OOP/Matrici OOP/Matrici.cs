290633c8abc1fbb62c7277969318065bd0f8f973
Madalina Gusu <58728913+madaandalina@users.noreply.github.com>
2020-06-03 17:18:40 +0300

    06.05.2020

---
+++ Matrici OOP/Matrici OOP/Matrici.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrici_OOP
+{
+    internal class Matrici
+    {
+        //campurile membre
+        private int randuri;
+        private int coloane;
+        private float[,] data;
+        private int cifre = 1;
+
+        public Matrici(int randuri, int coloane)
+        {
+            this.randuri = randuri;
+            this.coloane = coloane;
+            data = new float[this.randuri, this.coloane];
+        }
+
+        //constructorul default
+        public Matrici(int n) : this(n, n)
+        {
+
+        }
+
+        private static Random rnd = new Random();
+        public void CreateMatrice()
+        {
+            for (int i = 0; i < this.randuri; i++)
+            {
+                for (int j = 0; j < this.coloane; j++)
+                {
+                    data[i, j] = Matrici.rnd.Next(10);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            string formatString = "{0 ," + cifre + " }";
+
+            for (int i = 0; i < this.randuri; i++)
+            {
+                for (int j = 0; j < this.coloane; j++)
+                {
+                    sb.AppendFormat(formatString, data[i, j]);
+                    sb.Append(" ");
+                }
+                sb.Append(Environment.NewLine);
+            }
+
+            return sb.ToString();
+        }
+
+
+        //Adunarea a doua matrici
+        public Matrici Aduna(Matrici m2)
+        {
+            if (this.randuri != m2.randuri && this.coloane != m2.coloane)
+            {
+                throw new ImpossibleMatrixOperationException();
+            }
+
+            int n = this.randuri;
+            int m = this.coloane;
+
+            Matrici a = new Matrici(n, m);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    a.data[i, j] = this.data[i, j] + m2.data[i, j];
+                }
+            }
+
+            return a;
+        }
+
+        //scaderea a doua matrici
+        public Matrici Scade(Matrici m2)
+        {
+            if (this.randuri != m2.randuri && this.coloane != m2.coloane)
+            {
+                throw new ImpossibleMatrixOperationException();
+            }
+
+            int n = this.randuri;
+            int m = this.coloane;
+
+            Matrici a = new Matrici(n, m);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    a.data[i, j] = this.data[i, j] - m2.data[i, j];
+                }
+            }
+
+            return a;
+        }
+
+
+
+        public Matrici Inmulteste(Matrici m2)
+        {
+            if (this.coloane != m2.randuri)
+            {
+                throw new ImpossibleMatrixOperationException();
+            }
+            int n = this.randuri;
+            int m = m2.coloane;
+
+            Matrici a = new Matrici(n, m);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    float suma = 0;
+                    for (int k = 0; k < this.coloane; k++)
+                    {
+                        suma += this.data[i, k] * m2.data[k, j];
+                    }
+                    a.data[i, j] = suma;
+                }
+            }
+
+            return a;
+        }
+
+       
+  
+
+        public Matrici Putere(int n)
+        {
+            Matrici a = new Matrici(this.randuri, this.coloane);
+            while (n > 0)
+            {
+                a = this.Inmulteste(this);
+                n--;
+            }
+            return a;
+        }
+
+       
+      
+
+     
+    }
+}
---
+++ Matrici OOP/Matrici OOP/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrici_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //afisarile matricelor
+            Matrici a1 = new Matrici(2, 2);
+            a1.CreateMatrice();  
+            Console.WriteLine($"Afisarea matricii a1 \n{a1}");
+
+
+            Matrici a2 = new Matrici(2, 3);
+            a2.CreateMatrice();
+            Console.WriteLine($"Afisarea matricii a2 \n{a2}");
+
+            Matrici a3 = new Matrici(3, 3);
+            a3.CreateMatrice();
+            Console.WriteLine($"Afisarea matricii a3 \n{a3}");
+
+            Matrici a4 = new Matrici(3, 3);
+            a4.CreateMatrice();
+            Console.WriteLine($"Afisarea matricii a4 \n{a4}");
+
+            //operatiile cu matrici
+
+            Console.WriteLine($"Adunarea a doua matrici: ");
+            Matrici a5 = a3.Aduna(a4);
+            Console.WriteLine(a5);
+
+            
+            Console.WriteLine($"Scaderea a doua matrici: ");
+            Matrici a6 = a3.Scade(a4);
+            Console.WriteLine(a6);
+
+            Console.WriteLine($"Inmultirea a doua matrici: ");
+            Matrici a7 = a1.Inmulteste(a2);
+            Console.WriteLine(a7);
+
+            Console.WriteLine($"Ridicarea la putere a doua matrici: ");
+            Matrici a8 = a1.Putere(3);
+            Console.WriteLine(a8);
+
+
+            Console.ReadLine();
+        }
+    }
+}
