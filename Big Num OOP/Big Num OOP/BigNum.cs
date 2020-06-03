7428b9c8c8ffd59874917c20eba9f630fa54aa55
Madalina Gusu <58728913+madaandalina@users.noreply.github.com>
2020-06-03 14:28:25 +0300

    Add files via upload
    trimis pe platforma in data de 28 April 2020

---
+++ Big Num OOP/Big Num OOP/BigNum.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Big_Num_OOP
+{
+    class BigNum
+    {
+        //campurile
+        private string numar = "0";
+        private int cifre;
+
+        //proprietate read only
+        public int Cifre
+        {
+            get
+            {
+               return cifre;
+            }
+        }
+
+
+        //constructorul cu un parametru
+        public BigNum(string numar)
+        {
+            this.numar = numar;
+            cifre = numar.Length;
+        }
+
+       
+
+        public int this[int i]
+        {
+            get
+            {
+                return numar[i] - '0';
+            }
+        }
+
+        
+        public static BigNum operator +(BigNum x, BigNum y)
+        {
+            int litAddcifre, bigAddcifre;
+            BigNum littleAdd, bigAdd;
+
+            if (x.cifre < y.cifre)
+            {
+                litAddcifre = x.cifre;
+                bigAddcifre = y.cifre;
+                littleAdd = x;
+                bigAdd = y;
+            }
+            else
+            {
+                litAddcifre = y.cifre;
+                bigAddcifre = x.cifre;
+                littleAdd = y;
+                bigAdd = x;
+            }
+
+            int index = bigAddcifre - 1, carry = 0, sumOfTwoDigits;
+            string sum = "";
+
+            while (index > -1)
+            {
+                int indexForLitAdd = litAddcifre - (bigAddcifre - index);
+
+                sumOfTwoDigits = indexForLitAdd > -1
+                    ? bigAdd[index] + littleAdd[indexForLitAdd] + carry
+                    : bigAdd[index] + carry;
+
+                if (sumOfTwoDigits >= 10)
+                {
+                    carry = 1;
+                    sumOfTwoDigits -= 10;
+                }
+                else
+                {
+                    carry = 0;
+                }
+
+                index--;
+
+                sum = sumOfTwoDigits + sum;
+            };
+
+            if (carry == 1)
+                sum = "1" + sum;
+
+            return new BigNum(sum);
+        }
+        
+
+
+        public static BigNum operator *(BigNum x, BigNum y)
+        {
+            int n = y.cifre;
+            string stepper = "";
+            BigNum sum, product = new BigNum("0");
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int m = y[i];
+
+                sum = new BigNum("0");
+
+                for (int j = 0; j < m; j++)
+                    sum += x;
+
+                product += new BigNum(Convert.ToString(sum) + stepper);
+                stepper += "0";
+            }
+
+            return product;
+        }
+
+
+        //suprascrierea ToString
+        public override string ToString()
+        {
+            return numar;
+        }
+    }
+
+
+}
+
+
+
+
+    
+
---
+++ Big Num OOP/Big Num OOP/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Big_Num_OOP
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            /*Sa se creeze un tip de date denumit NumarMare pentru care se vor implementa operatorii + si * (de adunare si respectiv inmultire a numerelor foarte mari). Sa se utilizeze acest tip de date pentru determinarea:
+
+                celui de al 100-lea termen din sirul lui Fibonacci 
+                (primii doi termeni au valoarea 1, iar incepand de la al treilea termen, fiecare este suma anteriorilor doi termeni ai sai: 1, 1, 2, 3, 5, 8, 13, 21, ...)
+                valorii 1000! (factorial).
+            */
+
+
+            BigNum big1 = new BigNum("1234567891234567891234");
+            BigNum big2 = new BigNum("8888888888888888888888");     
+            BigNum big3 = big1 + big2;
+            Console.WriteLine("adunarea lui " + big1 + " cu " + big2 + " " + big3);
+
+            BigNum big4 = big1 * big2;
+            Console.WriteLine("inmultirea lui " + big1 + " cu " + big2 + " " + big4);
+
+
+            Console.WriteLine();
+            BigNum[] sirulFib = Fibonacci(100);
+            Console.WriteLine("Cel de-al 100-lea element al sirului lui Fibonacci este: " + sirulFib[100]);
+
+
+
+
+            Console.ReadLine();
+        }
+        static BigNum[] Fibonacci(int n)
+        {
+            BigNum[] sirulFib = new BigNum[n + 1];
+
+            sirulFib[0] = new BigNum("0");
+
+            if (n > 0)
+            {
+                sirulFib[1] = new BigNum("1");
+            }
+
+            if (n > 1)
+            {
+                BigNum a = new BigNum("0");
+                BigNum b = new BigNum("1");
+                BigNum aux;
+
+                for (int i = 2; i <= n; i++)
+                {
+                    aux = a;
+                    a = b;
+                    b = b + aux;
+                    sirulFib[i] = b;
+                }
+            }
+
+            return sirulFib;
+        }
+    }
+}
