05a53b7ad265bd3849e5353ae05912a3eb941688
Madalina Gusu <58728913+madaandalina@users.noreply.github.com>
2020-06-03 16:51:39 +0300

    03.06.2020

---
+++ playing card/Program (1)/ConsoleApp69/ConsoleApp69/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card c = new Card(rank.ace, suit.spade);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+        public enum rank
+        {
+            ace,two,three,four,five,six,seven,eight,nine,ten,jack,queen,king
+        }
+        public enum suit
+        {
+            spade,heart,diamond,club
+        }
+        public class Card
+        {
+            private readonly rank Rank;
+            private readonly suit Suit;
+            public Card(rank Rank, suit Suit)
+            {
+                this.Rank = Rank;
+                this.Suit = Suit;
+            }
+            public override string ToString()
+            {
+                return "Card: " + Rank + " of " + Suit;
+            }
+        }
+    }
+}
---
+++ playing card/playing card/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playing_card
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carte c = new Carte(ordine.asul, tip.inimaNeagra);
+            Carte c2 = new Carte(ordine.sase, tip.trefla);
+            Console.WriteLine(c);
+            Console.WriteLine(c2);
+            Console.ReadLine();
+        }
+        public enum ordine
+        {
+            asul, doi, trei, patru, cinci, sase, sapte, opt, noua, zece, juvete, dama, rege
+        }
+        public enum tip
+        {
+            inimaRosie,caro,trefla,inimaNeagra
+        }
+
+        class Carte
+        {
+            private ordine Ordine;
+            private tip Tip;
+            public Carte(ordine Ordine, tip Tip)
+            {
+                this.Ordine = Ordine;
+                this.Tip = Tip;
+            }
+            public override string ToString()
+            {
+                return $"{Ordine} de {Tip}";
+            }
+        }
+
+
+    }
+}
